78e7bfedbf1c8b798baad65e21e9af9cf5efc301
Dreary <ItsJustDreary@gmail.com>
2019-02-10 15:17:53 -0500

    React App Attempt

---
+++ precache-manifest.07d4a27631f746cb3a1d634aaf089785.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1d9d3c557d5efa87896a",
+    "url": "/static/js/main.1d9d3c55.chunk.js"
+  },
+  {
+    "revision": "585aed5fa65aa25653e5",
+    "url": "/static/js/1.585aed5f.chunk.js"
+  },
+  {
+    "revision": "1d9d3c557d5efa87896a",
+    "url": "/static/css/main.d4ccfac4.chunk.css"
+  },
+  {
+    "revision": "71bd594011d3cefecfed25f52ff7be71",
+    "url": "/index.html"
+  }
+];
